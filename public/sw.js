b2c7204e1612d63902e22f3ccf9e303fd9a6e76e
Amelie Ender <amelie.ender@gmx.de>
2021-05-06 12:12:14 +0200

    fix sw and add install to plus-button

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,21 @@
 self.addEventListener('install', event => event.waitUntil(
     caches.open('sw-cache-v1')
-    .then(cache => cache.add('/'))
+        .then(cache => cache.add('/'))
 ));
-   
+
+self.addEventListener('fetch', event => event.respondWith(
+    caches.open('sw-chache-v1')
+        .then(cache => cache.match(event.request))
+        .then(response => response || fetch(event.request))
+));
 
 // self.addEventListener('activate', event => {
 //     console.log('service worker --> activating ...', event);
 //     return self.clients.claim();
 // })
 
-self.addEventListener('fetch', event => event.respondWith(
-    caches.open('sw-cache-v1')
-    .then(cache => cache.match(event.request))
-    .then(response => response)
-));
+// self.addEventListener('fetch', event => event.respondWith(
+//     caches.open('sw-cache-v1')
+//     .then(cache => cache.match(event.request))
+//     .then(response => response)
+// ));
