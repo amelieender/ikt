96139901e9c05bc0a1dd7a69eebd183184183186
Amelie Ender <amelie.ender@gmx.de>
2021-05-17 16:30:03 +0200

    add dynamic caching

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+const CURRENT_STATIC_CACHE = 'static-v2';
+const CURRENT_DYNAMIC_CACHE = 'dynamic-v2';
+
 self.addEventListener('install', event => { 
     console.log('service worker --> installing ...', event);
     event.waitUntil(
-        caches.open('static')
+        caches.open(CURRENT_STATIC_CACHE)
             .then( cache => {
                 console.log('Service-Worker-Cache erzeugt und offen');
                 cache.addAll([
@@ -23,17 +26,39 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
     console.log('service worker --> activating ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then( keyList => {
+                return Promise.all(keyList.map( key => {
+                    if(key !== CURRENT_STATIC_CACHE && key !== CURRENT_DYNAMIC_CACHE) {
+                        console.log('service worker --> old cache removed :', key);
+                        return caches.delete(key);
+                    }
+                }))
+            })
+    );
     return self.clients.claim();
 })
 
 self.addEventListener('fetch', event => {
+    // check if request is made by chrome extensions or web page
+    // if request is made for web page url must contains http.
+    if (!(event.request.url.indexOf('http') === 0)) return; // skip the request if request is not made with http protocol
+
     event.respondWith(
         caches.match(event.request)
             .then( response => {
                 if(response) {
                     return response;
                 } else {
-                    return fetch(event.request);
+                    return fetch(event.request)
+                    .then(res => {
+                        return caches.open(CURRENT_DYNAMIC_CACHE)
+                        .then( cache => {
+                            cache.put(event.request.url, res.clone());
+                            return res;
+                        })
+                    });
             }
         })
     );
