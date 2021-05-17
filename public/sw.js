f453f4942410bee65d97dbe196cc0dc38e6984e3
Amelie Ender <amelie.ender@gmx.de>
2021-05-17 15:56:49 +0200

    add static caching

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,42 @@
-self.addEventListener('install', event => 
-    // event.waitUntil(
-    // caches.open('sw-cache-v1')
-    //     .then(cache => cache.add('/'))
-    {console.log('service worker --> installing ...', event);}
-)
+self.addEventListener('install', event => { 
+    console.log('service worker --> installing ...', event);
+    event.waitUntil(
+        caches.open('static')
+            .then( cache => {
+                console.log('Service-Worker-Cache erzeugt und offen');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/htw.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
+                ]);
+            })
+    );
+})
 
 self.addEventListener('activate', event => {
     console.log('service worker --> activating ...', event);
     return self.clients.claim();
 })
 
-self.addEventListener('fetch', event => 
-//     event.respondWith(
-//     caches.open('sw-chache-v1')
-//         .then(cache => cache.match(event.request))
-//         .then(response => response || fetch(event.request))
-// ));
-    {event.respondWith(fetch(event.request));
-    })
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then( response => {
+                if(response) {
+                    return response;
+                } else {
+                    return fetch(event.request);
+            }
+        })
+    );
+})
 
 
